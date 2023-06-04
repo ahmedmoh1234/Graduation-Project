7bb749ce5ee1ebbe0ed2e11663f0a1fe19032c14
Mostafa <Mostafa.Ashraf@goldensetanalytics.com>
2023-06-04 23:57:39 +0300

    [FEAT] Trial to improve accuracy of clothes module

--- Clothes Description Module/YOLO Clothes Detection and classification/Clothes_Description_module.py
+++ Clothes Description Module/YOLO Clothes Detection and classification/Clothes_Description_module.py
@@ -1,31 +1,60 @@
-from ultralytics import YOLO
-from tensorflow.keras.models import load_model
-import numpy as np
-import inflect
-import os
-import pickle
-import cv2
-from matplotlib import pyplot as plt
 from feature_selection.feature_selection import FeatureSelector
 from feature_extraction.feature_extraction import FeatureExtractor
-from sklearn.cluster import KMeans
-from webcolors import rgb_to_name
+from imports import *
 
 import warnings
 warnings.filterwarnings('ignore')
 
-
+clothes_to_segmentation_classes = {
+    "trousers": [6],
+    "coat": [4],
+    "jacket": [4],
+    "belt": [8],
+    "necklace": [],
+    "vest": [4],
+    "ring": [],
+    "shirt": [4],
+    "hat": [1],
+    "bracelet": [],
+    "tie": [],
+    "bag": [16],
+    "watch": [],
+    "scarf": [17],
+    "boot": [9, 10],
+    "sportshoes": [9, 10],
+    "sunglasses": [3],
+    "earrings": [],
+    "skirt": [5],
+    "dress": [7],
+    "socks and tight": [],
+    "shoes": [9, 10],
+    "underwear": [],
+    "backpack": [16],
+    "short": [6],
+    "night morning": [],
+    "gloves and mitten": [],
+    "wallet and purse": [],
+    "cufflinks": [],
+    "swimwear": [],
+    "makeup": [],
+    "suitcase": [16],
+    "jumpsuit": [7],
+    "suspenders": [],
+    "pouchbag": [16]
+}
 class ClothesDescriptor():
     
     def __init__(self):
         self.p = inflect.engine()
         self.texture_model = load_model("texture_ann.h5")
         self.texture_pca = pickle.load(open("texture_pca.pkl", "rb"))
+        self.segmentation_model = pickle.load(open("clothes_segmentation_model.pkl", "rb"))
         self.texture_extracted_features_train_mean = np.load("texture_extracted_features_train_mean.npy")
         self.texture_extracted_features_train_std = np.load("texture_extracted_features_train_std.npy")
         
         self.feature_extractor = FeatureExtractor()
         self.feature_selector = FeatureSelector()
+        self.segmentation_extractor = AutoFeatureExtractor.from_pretrained("mattmdjaga/segformer_b2_clothes")
       
         with open(self.createAbsolutePaths("/clothes_classes.pkl"), "rb") as f:
             self.clothes_classes = pickle.load(f)
@@ -49,64 +78,61 @@ class ClothesDescriptor():
             return "Cannot detect any cloth in the image."
         detected_clothes = []
         
+        segmentation_inputs = self.segmentation_extractor(images=image, return_tensors="pt")
+        
+        segmentation_outputs = self.segmentation_model(**segmentation_inputs)
+        logits = segmentation_outputs.logits.cpu()
+        upsampled_logits = nn.functional.interpolate(
+            logits,
+            size=image.shape[:2],
+            mode="bilinear",
+            align_corners=False,
+        )
+        
+        segmented_image = upsampled_logits.argmax(dim=1)[0]
+        
         for i in range(len(bboxes)):
             class_id = class_ids[i]
             detected_object = self.clothes_classes[class_id]
             
             cloth_box_image = image[bboxes[i][1]:bboxes[i][3], bboxes[i][0]:bboxes[i][2],:] # 3D array (width, height, channels)
             
-            roi = cloth_box_image # 3D array (width, height, channels)
-            rows, cols, _ = roi.shape
-            flattened_roi = roi.reshape(rows * cols, -1) # 2D array (rows*cols, channels)
-            
-            k = 2  # Number of clusters
-            kmeans = KMeans(n_clusters=k, random_state=0)
-            labels = kmeans.fit_predict(flattened_roi) # 1D array (rows*cols,)
-
-            _, cluster_sizes = np.unique(labels, return_counts=True)
-
-            cloth_segment = np.argmax(cluster_sizes) # 0 or 1
-
-            mask = np.zeros((rows * cols,), dtype=np.uint8) # 1D array (rows*cols,)
-            mask[labels == cloth_segment] = 255
-            mask = mask.reshape(rows, cols) # 2D array (rows, cols)
-            
-            kernel = np.ones((10,10),np.uint8)
-            mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-
-            segmented_cloth = cv2.bitwise_and(roi, roi, mask=mask) # 3D array (width, height, channels)
-            
-            square_min_x, square_min_y, square_max_x, square_max_y = self.find_largest_square(mask)
-            cloth_region = segmented_cloth[square_min_y:square_max_y, square_min_x:square_max_x,:] # 3D array (width, height, channels)
-            
-            color_val = cloth_region[cloth_region != 0]
-            
-            if color_val.size != 0:
-                 color_val = [color_val[0],color_val[0],color_val[0]]
-            else:
-                color_val = np.random.randint(0,255,3)
-             
-            # Replace pixels with 0 mask value
-            zero_pixels = np.where(mask[square_min_y:square_max_y, square_min_x:square_max_x] == 0)
-            cloth_region[zero_pixels] = color_val
+            segmented_cloth = self.select_cloth_pixels(segmented_image, image, detected_object)
                           
-            color = self.detect_color(cloth_region)
-
-            flag = True
-            increased_size = 8
-            texture = ""
-            count_trials = 20
+            color = self.detect_color(segmented_cloth)
             
-            while flag:
-                try:            
-                    texture = self.detect_texture(cloth_region)
-                    flag = False
-                except:
-                    cloth_region = segmented_cloth[square_min_y - increased_size:square_max_y+increased_size, square_min_x - increased_size:square_max_x + increased_size,:] # 3D array (width, height, channels)   
-                    increased_size += 8
-                    count_trials -= 1
-                    if count_trials == 0:
-                        break
+            segmentation_inputs = self.segmentation_extractor(images=cloth_box_image, return_tensors="pt")
+            
+            segmentation_outputs = self.segmentation_model(**segmentation_inputs)
+            logits = segmentation_outputs.logits.cpu()
+            upsampled_logits = nn.functional.interpolate(
+                logits,
+                size=cloth_box_image.shape[:2],
+                mode="bilinear",
+                align_corners=False,
+            )
+        
+            segmented_box = upsampled_logits.argmax(dim=1)[0]
+            
+            segmented_box_image = self.select_cloth_pixels(segmented_box, cloth_box_image, detected_object)
+            
+            segmented_box_binary =  np.where(segmented_box_image == 0, 0, 255)[:,:,0].astype(np.uint8)
+            plt.imshow(segmented_box_binary)
+            plt.show()
+            print(f"segmented_box_binary shape: {segmented_box_binary.shape}")
+            
+            square_min_x, square_min_y, square_max_x, square_max_y = self.find_largest_square(segmented_box_binary)
+            cloth_region = cloth_box_image[square_min_y:square_max_y, square_min_x:square_max_x,:] # 3D array (width, height, channels)
+            plt.imshow(cloth_region)
+            plt.show()
+
+            texture = ""
+            
+            try:            
+                texture = self.detect_texture(cloth_region)
+            except:
+                texture= ""
+
             
             detected_clothes.append((detected_object,texture,color))
         
@@ -123,7 +149,14 @@ class ClothesDescriptor():
                     result += detected_clothes[i][2] + " " + detected_clothes[i][1] + " " + detected_clothes[i][0] + "; "
         print(result)
         return result, detected_clothes
-        
+    
+    def select_cloth_pixels(self, segmented_image, original_image, cloth_class):
+        cloth_class_ids = clothes_to_segmentation_classes[cloth_class]
+        cloth_mask = np.isin(segmented_image, cloth_class_ids)
+        cloth_pixels = original_image.copy()
+        cloth_pixels[~cloth_mask] = 0
+        return cloth_pixels
+    
     def detect_texture(self,image):
         
         gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
@@ -149,17 +182,20 @@ class ClothesDescriptor():
     def detect_color(self, image):
 
         unique_colors, counts = np.unique(image.reshape(-1, image.shape[-1]), axis=0, return_counts=True)
+        temp = unique_colors.copy()
+        unique_colors = unique_colors[np.where(np.sum(temp, axis=1) != 0)]
+        counts = counts[np.where(np.sum(temp, axis=1) != 0)]
         
         sorted_indices = np.argsort(counts)[::-1]
         unique_colors = unique_colors[sorted_indices]
         counts = counts[sorted_indices]
-        dominant_colors = []
+        dominant_colors = set()
         
         for i in range(len(unique_colors)):
             try:
                 color = unique_colors[i]
-                named_color = rgb_to_name(tuple(color), spec='css3')
-                dominant_colors.append(named_color)
+                named_color = self.rgb_to_name(color)
+                dominant_colors.add(named_color)
                 if(len(dominant_colors) == 3):
                     break
             except ValueError:
@@ -169,7 +205,45 @@ class ClothesDescriptor():
 
         return dominant_colors_str
 
-    def find_largest_square(self,mask):
+    def rgb_to_name(self,rgb):
+        closest_color = None
+        min_distance = float('inf')
+        
+        # Predefined known colors
+        known_colors = [
+            'red',
+            'green',
+            'blue',
+            'yellow',
+            'orange',
+            'purple',
+            'pink',
+            'brown',
+            'gray',
+            'black'
+        ]
+        
+        for color_name in known_colors:
+            try:
+                color_rgb = webcolors.name_to_rgb(color_name)
+                distance = self.color_distance(rgb, color_rgb)
+                
+                if distance < min_distance:
+                    min_distance = distance
+                    closest_color = color_name
+            except ValueError:
+                continue
+        
+        return closest_color
+
+    def color_distance(self,rgb1, rgb2):
+        r1, g1, b1 = rgb1
+        r2, g2, b2 = rgb2
+        
+        distance = ((r2 - r1) ** 2) + ((g2 - g1) ** 2) + ((b2 - b1) ** 2)
+        return distance
+
+    def find_largest_square(self, mask):
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         largest_contour = max(contours, key=cv2.contourArea)
@@ -178,23 +252,11 @@ class ClothesDescriptor():
         box = cv2.boxPoints(rect)
         box = np.int0(box)
 
-        min_x = max(np.min(box[:, 0]),0)
-        max_x = None
-        
-        
-        if( np.max(box[:, 0]) < 0):
-            max_x = mask.shape[1]
-        else: 
-            max_x = np.max(box[:, 0])
-            
-        min_y = max(np.min(box[:, 1]),0)
-        
-        max_y = None
-                
-        if( np.max(box[:, 1]) < 0):
-            max_y = mask.shape[0]
-        else: 
-            max_y = np.max(box[:, 1])
+        min_x = max(np.min(box[:, 0]), 0)
+        max_x = min(np.max(box[:, 0]), mask.shape[1])
+
+        min_y = max(np.min(box[:, 1]), 0)
+        max_y = min(np.max(box[:, 1]), mask.shape[0])
 
         return min_x, min_y, max_x, max_y
 
@@ -218,4 +280,6 @@ class YOLOSegmentation:
     
 clothes_detector = ClothesDescriptor()
 test_image = cv2.imread("./test3.jpg")
-clothes_detector.describe_cloth(test_image)
+result = clothes_detector.describe_cloth(test_image)
+print(result[0])
+
